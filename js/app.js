501e4f3dbeb9631811ceea1b43dc5478d336b00a
thisisernesto <ernestoresende@hotmail.com>
2020-02-06 02:24:56 -0300

    Animation added to the header line-starter

--- js/app.js
+++ js/app.js
@@ -20,3 +20,22 @@ const navSlide=()=> {
 }
 
 navSlide();
+
+//Typing animation for the hero section
+const typedTextSpan = document.querySelector('.hero-line-animated');
+
+const textArray = ['Hi there, my name is'];
+const typingDelay = 60;
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if(charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+}
+
+type();
